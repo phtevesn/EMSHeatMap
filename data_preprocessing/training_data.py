5998f7df65b751aa2a6fca22dbfcd43e9f8bc254
West04 <west@finleysound.com>
2025-10-18 13:13:45 -0700

    refactoring: adding local file reading

--- data_preprocessing/training_data.py
+++ data_preprocessing/training_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from pathlib import Path
+
 from emt_data import get_emt_data
 from weather_data import get_weather_data
 from match_weather_data import match_weather_data
@@ -5,9 +8,15 @@ from add_non_emergency import add_non_emergency
 from grid import find_cells
 
 levels = 4
+RAW_EMT_DATA_PATH = '../data/ready_emt_data.parquet'
+
 
 def get_training_data():
-    emt_data = get_emt_data()
+    if Path(RAW_EMT_DATA_PATH).exists():
+        emt_data = pd.read_parquet(RAW_EMT_DATA_PATH)
+    else:
+        emt_data = get_emt_data()
+
     emt_data = find_cells(emt_data, levels)
     emt_data = add_non_emergency(emt_data)
     weather_data = get_weather_data()
