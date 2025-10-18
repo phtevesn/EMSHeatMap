c3cbb20a09afd74c7d87c081ebf0f3a29928bd65
W_Sammy <wongsammy23@gmail.com>
2025-10-18 12:47:07 -0700

    Refactor: Displayed map is now a Folium heatmap

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
+import folium
 from numpy.random import default_rng as rng
 from folium.plugins import HeatMap
+from streamlit_folium import st_folium
 
 st.set_page_config(
     page_title="EMS Prediction Atlas",
@@ -14,11 +16,16 @@ sf_df = pd.DataFrame(
     rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
     columns=["lat", "lon"],
 )
+sf_heat = folium.Map(location=[37.76, -122.4], zoom_start=12)
+HeatMap(sf_df[['lat', 'lon']].values, radius=10).add_to(sf_heat)
+
 # New York dummy-data map
 ny_df = pd.DataFrame(
     rng(0).standard_normal((1000, 2)) / [50, 50] + [40.7128, -74.0060],
     columns=["lat", "lon"],
 )
+ny_heat = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
+HeatMap(ny_df[['lat', 'lon']].values, radius=10).add_to(ny_heat)
 
 # Title
 st.markdown("<h1 style='text-align: left;'>EMS Prediction Atlas</h1>", unsafe_allow_html=True)
@@ -34,7 +41,7 @@ notes, maps = st.columns([1,3], gap="small")
 if selected_city == "San Francisco":
     with maps:
         st.markdown("<h1 style='text-align: center;'>Heat Map</h1>", unsafe_allow_html=True)
-        st.map(sf_df)  
+        st_folium(sf_heat, width=1120, height=500)  
 
     with notes:
         st.markdown("<h1 style='text-align: center;'>Information</h1>", unsafe_allow_html=True)
@@ -43,7 +50,7 @@ if selected_city == "San Francisco":
 elif selected_city == "New York":
     with maps:
         st.markdown("<h1 style='text-align: center;'>Heat Map</h1>", unsafe_allow_html=True)
-        st.map(ny_df)  
+        st_folium(ny_heat, width=1120, height=500)  
 
     with notes:
         st.markdown("<h1 style='text-align: center;'>Information</h1>", unsafe_allow_html=True)
